dee933fb63d3a79d6657bf5cec5b1ae32b353127
Vishnu Vijayasankar <vishnu.vijayasankar@maybank.com>
2020-06-20 19:42:56 +0530

    Updates

---
+++ precache-manifest.e13ec3027e4d9b7fdb562a7f497b2bbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7630ae9968af85379b5aa3e60029d3",
+    "url": "/github-ui/index.html"
+  },
+  {
+    "revision": "5878e02283945c8d2844",
+    "url": "/github-ui/static/css/main.37d0fd80.chunk.css"
+  },
+  {
+    "revision": "794d8fb453b4bb19c74d",
+    "url": "/github-ui/static/js/2.6c05f1e2.chunk.js"
+  },
+  {
+    "revision": "d86f3a7a1e3c7f8afbd22066d3fc3c72",
+    "url": "/github-ui/static/js/2.6c05f1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5878e02283945c8d2844",
+    "url": "/github-ui/static/js/main.213229b2.chunk.js"
+  },
+  {
+    "revision": "a7f5fbdfb0a8cdc5ebe8",
+    "url": "/github-ui/static/js/runtime-main.c9a3fb7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
